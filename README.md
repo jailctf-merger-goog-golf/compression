22b8308dbcabe4e5768c3bb730a0f4cb7db1c0e9
quasar098 <70716985+quasar098@users.noreply.github.com>
2025-08-06 22:13:56 -0400

    hopefully works

---
+++ README.md
@@ -0,0 +1,8 @@
+# put compression options in options directory
+
+input is in the file in sys.argv[1]
+
+stdout is sent to solution text box on ui
+
+stderr is put in a popup on ui
+
---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import tempfile
+from sys import argv
+
+def print_usage_and_exit():
+    print(f"""
+Usage: python3 {argv[0]} <list|run> [args...]
+
+Example:
+ - python3 {argv[0]} list
+ - python3 {argv[0]} run v1
+    """.strip())
+    exit(1)
+
+
+if len(argv) == 1:
+    print_usage_and_exit()
+
+OPTIONS_DIR = os.path.join(os.path.dirname(__file__), 'options')
+
+option = argv[1]
+if option == "list":
+    print("\n".join(["* " + l for l in os.listdir(OPTIONS_DIR)]))
+elif option == "run":
+    if len(argv) != 3:
+        print_usage_and_exit()
+    if not os.path.exists(os.path.join(OPTIONS_DIR, argv[2])+'.py'):
+        print('not found')
+        exit(1)
+    inp = bytes.fromhex(input('hex > '))
+    with tempfile.NamedTemporaryFile(suffix='.py') as f:
+        f.write(inp)
+        f.flush()
+        cmd='python3.11 '+os.path.join(OPTIONS_DIR, argv[2])+f'.py {f.file.name}'
+        os.system(cmd)
+
