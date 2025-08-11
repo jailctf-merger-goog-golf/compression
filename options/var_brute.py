8f2442bf82800387972b9f493d5cf02b2506b5bb
Nico Posada <posadanico@outlook.com>
2025-08-11 14:42:02 -0500

    Fix regex for var brute to work with function names that aren't p

--- options/var_brute.py
+++ options/var_brute.py
@@ -7,7 +7,7 @@ import re
 # possible single letter varnames to use
 VARNAMES = list("abcdefghijklmnopqrstuvwxyz")
 
-VAR_PAT = re.compile(r"(?<!def )\b(?<!\"|')[a-zA-Z_](?!\"|')\b")
+VAR_PAT = re.compile(r"\b(?<!\"|')(?!p\()[a-zA-Z_](?!\"|')\b(?<!def p)")
 
 def do_rand_compress(code, check_syntax=False):
     global VARNAMES, VAR_PAT
@@ -29,23 +29,30 @@ def do_rand_compress(code, check_syntax=False):
     
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", category=SyntaxWarning)
-        compressed_code = get_compressed(brute_code, max_brute=3_000, use_tqdm=False, check_syntax=check_syntax)
+        compressed_code = get_compressed(brute_code, max_brute=1_000, use_tqdm=False, check_syntax=check_syntax)
 
-    return brute_code, compressed_code
+    return brute_code, compressed_code, mapping
 
 def do_brute(code: str | bytes, iterations: int, use_tqdm=True, log_best=True):
+    global VARNAMES
     code = code.strip()
+    
+    _, _, mapping = do_rand_compress(code, False)
+    VARNAMES = list("abcdefghijklmnopqrstuvwxyz")#[1:len(mapping)+1]
     
     best_compressed = None
     best_bruted = None
     for i in (trange if use_tqdm else range)(iterations):
         # only check syntax on the first 10 to try to catch any outlier issues.
         # If that passes we stop checking syntax to speed it up
-        bruted, compressed = do_rand_compress(code, i<10)
+        bruted, compressed, _ = do_rand_compress(code, i<10)
         if best_compressed is None or len(compressed) < len(best_compressed):
             if log_best:
-                print(f"New Best {len(compressed)}!")
+                print(f"New best {len(code)}b => {len(compressed)}b!")
                 print(bruted)
+                print()
+                print(compressed.hex())
+                print('=' * 50)
             best_bruted = bruted
             best_compressed = compressed
 
@@ -53,18 +60,26 @@ def do_brute(code: str | bytes, iterations: int, use_tqdm=True, log_best=True):
 
 if __name__ == "__main__":
     code = r"""
-import re
-def p(a):
- b=[]
- for c in"0123456789".replace(d:=max(re.sub(", ","",str(a)),key=re.sub(", ","",str(a)).count),""):b+=re.findall(f"({c}+)(a*)({c}*)",re.sub(f'[{"0123456789".replace(c,"")}]',"a",re.sub(", ","",str(a))+re.sub(", ","",str([*zip(*a)]))));g={max([i,k],key=len)+d*len(j)+max([i,k],key=len) if k else i for i,j,k in b if{len(i),len(k)}-{1}};g=[s if len(s)!=2else s[0]*3for s in sorted({s for s in g if str(g).count(s)<2},key=len)]
- g=[d]*(len(g[0])>1)+g
- o=[len(max(g,key=len))*[0]for s in len(max(g,key=len))*[0]]
- p=0
- while g:q=[int(s)for s in g.pop()];o[p][p:p+len(q)]=o[~p][p:p+len(q)]=q;p+=1
- return[[*map(max,zip(*s))]for s in zip(o,zip(*o))]
+def f(g,x,y,s):
+ for A in-1,0,1:
+  for B in-1,0,1:
+   if B|A!=0<=x+B<13>y+A>=0<g[y+A][x+B]and((x+B,y+A)in s)<1:s=f(g,x+B,y+A,s|{(x+B,y+A)})
+ return s
+def p(g):
+  m={}
+  for y,r in enumerate(g):
+   for x,v in enumerate(r):
+    if{v}&{3,2}:
+     if len(S:=f(g,x,y,set()))>1:m[v]=({(x-B,y-A,g[A][B])for B,A in S}-{(0,0,v)})
+  for y,r in enumerate(g):
+   for x,v in enumerate(r):
+    if{v}&{3,2}:
+     if len(S:=f(g,x,y,set()))<2:
+      for B,A,r in m[v]:g[y-A][x-B*(1|-(v==2))]=r
+  return g
 """
 
-    bruted, compressed = do_brute(code, 1000)
+    bruted, compressed = do_brute(code, 10_000)
 
     print("=" * 50)
     print(f"Best code | {len(bruted)}b => {len(compressed)}b\n{bruted}\n\n{compressed.hex()}")
